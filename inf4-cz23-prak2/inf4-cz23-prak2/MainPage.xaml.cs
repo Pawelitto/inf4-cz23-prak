1fd345258f815a8c806c22acab33017ed2973325
Paweł Pietras <pawel.pietras144@gmail.com>
2024-02-15 09:05:34 +0100

    app

--- inf4-cz23-prak2/inf4-cz23-prak2/MainPage.xaml.cs
+++ inf4-cz23-prak2/inf4-cz23-prak2/MainPage.xaml.cs
@@ -7,12 +7,36 @@ using System.Threading.Tasks;
 using Xamarin.Forms;
 
 namespace inf4_cz23_prak2
-{
+{ 
     public partial class MainPage : ContentPage
     {
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        public int responseState = 0;
+        public string[] responses = new string[] { "Dzień dobry", "Good Morning", "Buenos dias" };
+
+        void slider_ValueChanged(System.Object sender, Xamarin.Forms.ValueChangedEventArgs e)
+        {
+            int intValueSlider = (int)slider.Value;
+
+            sliderSize.Text = "Rozmiar: " + intValueSlider;
+            response.FontSize = intValueSlider;
+        }
+
+        void Button_Clicked(System.Object sender, System.EventArgs e)
+        {
+            if(responseState != 2)
+            {
+                responseState += 1;
+                response.Text = responses[responseState];
+            } else
+            {
+                responseState = 0;
+            }
+            
         }
     }
 }
